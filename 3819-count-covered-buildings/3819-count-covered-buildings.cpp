bd3135fe48922a2c5c315f715695d2fd6655b4e8
mittaltanmay <139030702+mittaltanmay@users.noreply.github.com>
2025-12-11 18:57:27 +0530

    3819-COUNT-COVERED-BUILDINGS Stats: Time: 580 ms (44.75%), Space: 390.9 MB (43.65%) - LeetHub

--- 3819-count-covered-buildings/3819-count-covered-buildings.cpp
+++ 3819-count-covered-buildings/3819-count-covered-buildings.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        unordered_map<int,vector<int>> col; // storing the y with all the x values  
-        unordered_map<int,vector<int>> row; 
+        unordered_map<int,vector<int>> col; // storing the col along with all the x values present in that column   
+        unordered_map<int,vector<int>> row; // storing the row along with all the y values present in that row 
         int m=buildings.size();
         for(int i=0;i<m;i++)
         {
@@ -24,10 +24,10 @@ public:
             int c=buildings[i][1];
             vector<int> &temp1=row[r];
             vector<int> &temp2=col[c];
-            int ind1=lower_bound(temp1.begin(),temp1.end(),c)-temp1.begin();
-            int ind2=lower_bound(temp2.begin(),temp2.end(),r)-temp2.begin();
+            int ind1=lower_bound(temp1.begin(),temp1.end(),c)-temp1.begin(); // where y value lies in the column value of row 
+            int ind2=lower_bound(temp2.begin(),temp2.end(),r)-temp2.begin(); // where x value lies in the row value of column
 
-            if((ind1>0 && ind1<temp1.size()-1) && (ind2>0 && ind2<temp2.size()-1)) ans++;
+            if((ind1>0 && ind1<temp1.size()-1) && (ind2>0 && ind2<temp2.size()-1)) ans++; // a point will be covered if it between the coordiantes for both row and column
         }
         return ans;
     }
